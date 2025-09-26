c350e5a93e8e5ede3500019136d53a3c70bd8cb1
Song <songyuyan2001@hotmail.com>
2025-09-26 16:05:50 -0700

    initial commit for julia-gpu, nothing is shown yet

--- common/cpu_bitmap.h
+++ common/cpu_bitmap.h
@@ -1,86 +1,112 @@
-/*
- * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
- *
- * NVIDIA Corporation and its licensors retain all intellectual property and
- * proprietary rights in and to this software and related documentation.
- * Any use, reproduction, disclosure, or distribution of this software
- * and related documentation without an express license agreement from
- * NVIDIA Corporation is strictly prohibited.
- *
- * Please refer to the applicable NVIDIA end user license agreement (EULA)
- * associated with this source code for terms and conditions that govern
- * your use of this NVIDIA software.
- *
- */
-
-
 #ifndef __CPU_BITMAP_H__
 #define __CPU_BITMAP_H__
 
-#include "gl_helper.h"
+#include <iostream>
+#include <memory>
+
+#include "glfw_helper.h"
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
 
 struct CPUBitmap {
-    unsigned char    *pixels;
-    int     x, y;
-    void    *dataBlock;
-    void (*bitmapExit)(void*);
+  std::unique_ptr<unsigned char> pixels;
+  int x = 0, y = 0;
+  void *dataBlock;
+  void (*bitmapExit)(void*);
 
-    CPUBitmap( int width, int height, void *d = NULL ) {
-        pixels = new unsigned char[width * height * 4];
-        x = width;
-        y = height;
-        dataBlock = d;
+  CPUBitmap(int width, int height, void *d = NULL) :
+    x(width), y(height), dataBlock(d) {
+    pixels.reset(new unsigned char[width * height * 4]);
+  }
+
+  ~CPUBitmap() = default;
+
+  unsigned char* get_ptr( void ) const   { return pixels.get(); }
+  long image_size( void ) const { return x * y * 4; }
+
+  void display_and_exit( void(*e)(void*) = NULL ) {
+      /*
+          CPUBitmap**   bitmap = get_bitmap_ptr();
+          *bitmap = this;
+          bitmapExit = e;
+          // a bug in the Windows GLUT implementation prevents us from
+          // passing zero arguments to glutInit()
+          int c=1;
+          char* dummy = "";
+          glutInit( &c, &dummy );
+          glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
+          glutInitWindowSize( x, y );
+          glutCreateWindow( "bitmap" );
+          glutKeyboardFunc(Key);
+          glutDisplayFunc(Draw);
+          glutMainLoop();
+          */
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    GLFWwindow *window = glfwCreateWindow(x, y, "bitmap", NULL, NULL);
+    if (window == NULL) {
+      std::cout << "Failed to create GLFW window" << std::endl;
+      glfwTerminate();
+      return;
     }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    ~CPUBitmap() {
-        delete [] pixels;
+    while (!glfwWindowShouldClose(window)) {
+      // input
+      processInput(window);
+
+      // render
+      Draw();
+
+      // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+      glfwSwapBuffers(window);
+      glfwPollEvents();
     }
+  }
 
-    unsigned char* get_ptr( void ) const   { return pixels; }
-    long image_size( void ) const { return x * y * 4; }
+  // static method used for glut callbacks
+  static CPUBitmap** get_bitmap_ptr( void ) {
+    static CPUBitmap *gBitmap;
+    return &gBitmap;
+  }
 
-    void display_and_exit( void(*e)(void*) = NULL ) {
-        CPUBitmap**   bitmap = get_bitmap_ptr();
-        *bitmap = this;
-        bitmapExit = e;
-        // a bug in the Windows GLUT implementation prevents us from
-        // passing zero arguments to glutInit()
-        int c=1;
-        char* dummy = "";
-        glutInit( &c, &dummy );
-        glutInitDisplayMode( GLUT_SINGLE | GLUT_RGBA );
-        glutInitWindowSize( x, y );
-        glutCreateWindow( "bitmap" );
-        glutKeyboardFunc(Key);
-        glutDisplayFunc(Draw);
-        glutMainLoop();
-    }
+  // static method used for glut callbacks
+  static void Key(unsigned char key, int x, int y) {
+        /*        switch (key) {
+                    case 27:
+                        CPUBitmap*   bitmap = *(get_bitmap_ptr());
+                        if (bitmap->dataBlock != NULL && bitmap->bitmapExit != NULL)
+                            bitmap->bitmapExit( bitmap->dataBlock );
+                        exit(0);
+                }*/
+  }
 
-     // static method used for glut callbacks
-    static CPUBitmap** get_bitmap_ptr( void ) {
-        static CPUBitmap   *gBitmap;
-        return &gBitmap;
-    }
-
-   // static method used for glut callbacks
-    static void Key(unsigned char key, int x, int y) {
-        switch (key) {
-            case 27:
-                CPUBitmap*   bitmap = *(get_bitmap_ptr());
-                if (bitmap->dataBlock != NULL && bitmap->bitmapExit != NULL)
-                    bitmap->bitmapExit( bitmap->dataBlock );
-                exit(0);
-        }
-    }
-
-    // static method used for glut callbacks
-    static void Draw( void ) {
-        CPUBitmap*   bitmap = *(get_bitmap_ptr());
-        glClearColor( 0.0, 0.0, 0.0, 1.0 );
-        glClear( GL_COLOR_BUFFER_BIT );
-        glDrawPixels( bitmap->x, bitmap->y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap->pixels );
-        glFlush();
-    }
+  // static method used for glut callbacks
+  static void Draw( void ) {
+    //CPUBitmap*   bitmap = *(get_bitmap_ptr());
+    //glClearColor( 0.0, 0.0, 0.0, 1.0 );
+    //glClear( GL_COLOR_BUFFER_BIT );
+    //glDrawPixels( bitmap->x, bitmap->y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap->pixels.get() );
+    //glFlush();
+  }
 };
 
 #endif  // __CPU_BITMAP_H__
